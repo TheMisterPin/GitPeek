a4a39fa36661c69e569d60a0c6807d3f7207c5cf
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2024-02-08 12:42:11 +0100

    Initial project setup with Vite and environment configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 'off',
+    semi: 'off', // Disable the core ESLint semi rule
+    '@typescript-eslint/semi': ['error', 'never'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'no-trailing-spaces': 'error',
+    'padded-blocks': [
+      'error',
+      { blocks: 'never', classes: 'never', switches: 'never' },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'function', next: 'function' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+
+    quotes: ['error', 'single'],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/prefer-default-export': 'off',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="read-the-docs">github Repos Lookup</p>
+}
+
+export default App
